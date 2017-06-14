a23ff4fd42f4c4325c5698d63a49f16d1c258d13
balark <balark.pro@gmail.com>
2017-06-14 11:03:15 +0530

    Create index.js

--- app/index.js
+++ app/index.js
@@ -1,14 +1,19 @@
-/*const express = require ('express')
-const app = express()
-const port = 3000
-app.get('/')
-*/
-var fs = require ('fs');
-var file = 'data.txt';
-console.log('code run');
-fs.readFile('./app/data.txt','utf8',function(error,data)
-                          {
-                            console.log(data);
-                          }
+// Main program execution app/index.js
+var Gpio = require('onoff').Gpio,
+  sensor = new Gpio(17, 'in', 'both');    //#A
+  var instance = 0 ;
 
-            );
+sensor.watch(function (err, value) { //#B
+  if (err) exit(err);
+  console.log('\033[2J');
+  if(value) instance++;
+  console.log(value ? 'Flame Detected, Instance :: '+instance : 'No Flame Detected');
+});
+
+function exit(err) {
+  if (err) console.log('An error occurred: ' + err);
+  sensor.unexport();
+  console.log('Bye, bye!')
+  process.exit();
+}
+process.on('SIGINT', exit);
